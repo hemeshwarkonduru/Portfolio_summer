dd05c9a531a7eefead8643d635136810a21e0fb0
MassKaBaap <hkonduru@iu.edu>
2023-07-01 01:01:46 -0400

    added tab name in index.html

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,11 +15,8 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Projects I've worked on
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
