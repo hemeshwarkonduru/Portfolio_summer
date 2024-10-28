44277dd77c9f4b3e1926292eb032c8c42c6a877a
MassKaBaap <hkonduru@iu.edu>
2024-10-28 14:45:01 -0400

    changes done to rectify build errors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/15581993_5643236.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
